e0157268a2760795da45ff84f27ae9d6343213ff
Андрей <andrey240101@gmail.com>
2021-07-23 14:53:22 +0300

    First commit

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group, Permission
 
 class StatusChoise(models.Model):
     """Статусы заявки"""
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name='Название')
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class StatusChoise(models.Model):
 
 class TypeRequest(models.Model):
     """Типы заявок"""
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, verbose_name='Тип')
 
     def __str__(self):
         return self.type
@@ -28,14 +28,14 @@ class TypeRequest(models.Model):
 
 class Requests(models.Model):
     """Заявки"""
-    name = models.CharField(max_length=100)
-    type = models.ForeignKey(TypeRequest, on_delete=models.CASCADE)
-    request = models.TextField()
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    type = models.ForeignKey(TypeRequest, on_delete=models.CASCADE, verbose_name='Тип заявки')
+    request = models.TextField(verbose_name='Заявка')
     email = models.EmailField()
-    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-    status = models.ForeignKey(StatusChoise, on_delete=models.CASCADE, blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, verbose_name='Менеджер')
+    status = models.ForeignKey(StatusChoise, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Статус')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Изменено')
 
     def __str__(self):
         return self.name
@@ -46,7 +46,7 @@ class Requests(models.Model):
 
 
 class UserRequest(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Имя')
     email = models.EmailField(unique=True)
 
     def __str__(self):
