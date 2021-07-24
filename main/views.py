924087d893aacdb5dbca5e251e1b993dc7eca4f9
Андрей <andrey240101@gmail.com>
2021-07-25 01:00:18 +0300

    First commit

--- main/views.py
+++ main/views.py
@@ -1,14 +1,11 @@
-import requests
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.db import IntegrityError
-from .forms import *
-from .models import UserRequest
 from uuid import uuid4
 from django.db.models.signals import post_save
-import time
+import requests
 
-
+from .forms import *
 
 
 def main(request):
@@ -18,10 +15,10 @@ def main(request):
             TOKEN = str(uuid4())
             cd = form.cleaned_data
             r = Requests(name=cd['name'],
-                        type=cd['type'],
-                        request=cd['request'],
-                        email=cd['email'],
-                        TOKEN=TOKEN)
+                         type=cd['type'],
+                         request=cd['request'],
+                         email=cd['email'],
+                         TOKEN=TOKEN)
             r.save()
             Requests.objects.update()
             try:
@@ -35,22 +32,20 @@ def main(request):
     return render(request, 'main/main.html', {'form': form})
 
 
-
+"""Уведомляем пользователя о статусе заявки"""
 def my_handler(sender, update_fields, instance, **kwargs):
-    sta ='status'
+    sta = 'status'
     print(instance.status)
     if sta in update_fields:
         status = instance.status
         api_key = settings.API
         chat_id = str(instance.bot_id)
-        text = str(status)
+        text = 'Статус вашей заявки изменился: ' + str(status)
         url = f'https://api.telegram.org/bot{api_key}/sendMessage'
         params = {
             'chat_id': chat_id,
             'text': text,
         }
         return requests.get(url, params=params).json()
-
-
 
 post_save.connect(my_handler, sender=Requests)
